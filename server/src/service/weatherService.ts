5f13d9e38404d196af7c457f7faf3e2fb1aa8da5
Daniel <dolaes@ucsc.edu>
2024-08-26 13:42:19 -0700

    Reset Weather Files

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -1,99 +1,31 @@
 import dotenv from 'dotenv';
-import axios from 'axios';
 dotenv.config();
 
 // TODO: Define an interface for the Coordinates object
-interface Coordinates {
-  xCoor: number;
-  yCoor: number;
-}
 
 // TODO: Define a class for the Weather object
-class Weather {
-  temperature: number;
-  wind: number;
-  humidity: number;
-
-  constructor(temperature: number, wind: number, humidity: number) {
-    this.temperature = temperature;
-    this.wind = wind;
-    this.humidity = humidity;
-  }
-}
 
 // TODO: Complete the WeatherService class
 class WeatherService {
   // TODO: Define the baseURL, API key, and city name properties
-  private baseURL?: string;
-  private apiKey?: string;
-  private cityName: string;
-
-  constructor() {
-    this.baseURL = process.env.API_BASE_URL || '';
-    this.apiKey = process.env.API_KEY || '';
-    this.cityName = '';
-  }
-
   // TODO: Create fetchLocationData method
-  private async fetchLocationData(query: string) {
-    const url = this.buildGeocodeQuery();
-    const response = await axios.get(url);
-    return response.data;
-  }
-
+  // private async fetchLocationData(query: string) {}
   // TODO: Create destructureLocationData method
-  private destructureLocationData(locationData: Coordinates): Coordinates {
-    return {
-      xCoor: locationData.xCoor,
-      yCoor: locationData.yCoor
-    };
-  }
-
+  // private destructureLocationData(locationData: Coordinates): Coordinates {}
   // TODO: Create buildGeocodeQuery method
-  private buildGeocodeQuery(): string {
-    return `${this.baseURL}?q=${encodeURIComponent(this.cityName)}&appid=${this.apiKey}`
-  }
-
+  // private buildGeocodeQuery(): string {}
   // TODO: Create buildWeatherQuery method
-  private buildWeatherQuery(coordinates: Coordinates): string {
-    return `${this.baseURL}/weather?lat=${coordinates.xCoor}&lon=${coordinates.yCoor}&appid=${this.apiKey}`;
-  }
-
+  // private buildWeatherQuery(coordinates: Coordinates): string {}
   // TODO: Create fetchAndDestructureLocationData method
-  private async fetchAndDestructureLocationData() {
-    const locationData = await this.fetchLocationData(this.cityName);
-    return this.destructureLocationData(locationData);
-  }
-
+  // private async fetchAndDestructureLocationData() {}
   // TODO: Create fetchWeatherData method
-  private async fetchWeatherData(coordinates: Coordinates) {
-    const url = this.buildWeatherQuery(coordinates);
-    const response = await axios.get(url);
-    return response.data;
-  }
-
+  // private async fetchWeatherData(coordinates: Coordinates) {}
   // TODO: Build parseCurrentWeather method
-  private parseCurrentWeather(response: any) {
-    const temperature = response.main.temp;
-    const wind = response.wind.speed;
-    const humidity = response.main.humidity;
-    return new Weather(temperature, wind, humidity);
-  }
-
+  // private parseCurrentWeather(response: any) {}
   // TODO: Complete buildForecastArray method
-  private buildForecastArray(currentWeather: Weather, weatherData: any[]) {
-    return [currentWeather];
-  }
-
+  // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
   // TODO: Complete getWeatherForCity method
-  async getWeatherForCity(city: string) {
-    this.cityName = city;
-    const coordinates = await this.fetchAndDestructureLocationData();
-    const weatherData = await this.fetchWeatherData(coordinates);
-    const currentWeather = this.parseCurrentWeather(weatherData);
-    return this.buildForecastArray(currentWeather, []);
-  }
-
+  // async getWeatherForCity(city: string) {}
 }
 
 export default new WeatherService();
